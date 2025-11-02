41f66a0067c4ad253a4f14f12a3b31a99bba3ec9
jhill172-create <jhill172@mail.cfcc.edu>
2025-11-02 10:40:13 -0500

    Add footer section to footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+    </main>
+    <footer class="site-footer">
+      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> — Theme by jhill172.</p>
+    </footer>
+  </div>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
